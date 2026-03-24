276a06c0b7bb8739647a9d02c3d6f52ea9ee2b87
luisgdoliveira-008-2213 <luis_gd_oliveira@estudante.sesisenai.org.br>
2026-03-24 08:08:05 -0300

    add - conclusão da ativ_6

---
+++ exercicio_06.js
@@ -0,0 +1,13 @@
+function vogaleconsoante() {
+
+let letra = prompt ("Digite uma letra");
+let vogais = [a,e,i,o,u,A,E,I,O,U];
+
+if(vogais.includes(letra)){
+    alert("vogal");
+}else{
+    alert("consoante");
+}
+
+
+}
