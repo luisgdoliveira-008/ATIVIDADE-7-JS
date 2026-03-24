35400ca56aa1f89a7d592a4b50a5b15ea8523d5e
luisgdoliveira-008-2213 <luis_gd_oliveira@estudante.sesisenai.org.br>
2026-03-24 09:49:30 -0300

    add - conclusão atividade 7

---
+++ exercicio_07.js
@@ -0,0 +1,32 @@
+function escolherPicole () {
+    let codigo = prompt("Digite o codigo do picole(de a até i)");
+
+    if(codigo == "a"){
+        console.log("Chocolate -- R$ 1,50")
+    } else if (codigo == "b") {
+        console.log ("Morango -- R$ 2,50")
+    } else if (codigo == "c"){
+        console.log ("Creme -- R$ 2,50")
+    } else if (codigo == "d"){
+       console.log ("Manga -- 3,20") 
+    } else if (codigo == "e"){
+        console.log("Melancia -- 3,40 ")
+    } else if (codigo == "f"){
+        console.log ("Vanilla")
+    } else if (codigo == "g"){
+        console.log ("Céu azul")
+    } else if (codigo == "h"){
+        console.log ("Brownie")
+    } else if (codigo =="i"){
+        console.log ("Hawaino")
+    }
+    }
+
+
+    
+
+
+            
+
+
+
