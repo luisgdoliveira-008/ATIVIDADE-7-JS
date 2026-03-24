131f7392b786debd61cefac51ae3e655e8753e9a
luisgdoliveira-008-2213 <luis_gd_oliveira@estudante.sesisenai.org.br>
2026-03-24 10:02:50 -0300

    add - atividade 08

---
+++ exercicio_08.js
@@ -0,0 +1,17 @@
+function variaveis(){
+
+    let num1 = 10;
+    let num2 = 5;
+
+    let diferenca = num1 - num2;
+    let resultadoComposto = (2 * num1) + (3 * num2);
+    let multiplicacao = num1 * num2;
+    
+    console.log(diferenca);
+    console.log(resultadoComposto);
+    console.log(multiplicacao);
+
+
+
+
+}
