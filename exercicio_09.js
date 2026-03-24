3a435341f917a36e0f08d2b57c9fa42308132b00
luisgdoliveira-008-2213 <luis_gd_oliveira@estudante.sesisenai.org.br>
2026-03-24 10:12:44 -0300

    add - conclusão da atividade 9

---
+++ exercicio_09.js
@@ -0,0 +1,17 @@
+function menorMaior(){
+
+let valor1 = 15;
+let valor2 = 42;
+
+console.log("do maior para o menor:");
+
+if (valor1 > valor2) {
+    console.log(valor1, valor2);
+} else if (valor2 > valor1) {
+    console.log(valor2, valor1);
+} else {
+    console.log("Os números são iguais:", valor1, "e", valor2);
+}
+
+
+}
